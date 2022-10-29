e270bb41e167c7b5e8fed81a6b1c616abd07a2fe
Eshita <eshita.sapariya@gmail.com>
2022-10-29 15:53:43 +0530

    defined theme for styled components and created few styled components

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { NavList, LinkStyled } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
   { to: '/starred', text: 'Starred' },
 ];
 const Navs = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(item => (
           <li>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
